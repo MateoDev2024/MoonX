08be38e6d0f831290e19e3b669170db8b1eb78f9
Mateo <159031831+MateoDev2024@users.noreply.github.com>
2025-04-11 22:44:31 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -10,6 +10,8 @@ elseif game.PlaceId == 116495829188952 then
 elseif game.PlaceId == 7606564092 then
     queue_on_teleport('loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bbccdf75425332848ae0bf6d3068f0c1.lua"))()')
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/bbccdf75425332848ae0bf6d3068f0c1.lua"))()
+elseif game.PlaceId == 85896571713843 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f5d517c7a5fe691a69c4f0c33c3bc514.lua"))()
 else
     warn("[Moon X] -> Game not supported.")
 end
